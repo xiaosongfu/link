98851362c6e2118034609b5077de4f219310508b
xiaosongfu <xiaosong.fu@outlook.com>
2018-05-17 11:44:46 +0800

    add logger func

--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 func main() {
 	// 配置路由
 	// --> category
-	http.HandleFunc("/api/v1/addCategory", route.AddCategory)
-	http.HandleFunc("/api/v1/getCategorys", route.GetCategorys)
+	http.HandleFunc("/api/v1/addCategory", logger(route.AddCategory))
+	http.HandleFunc("/api/v1/getCategorys", logger(route.GetCategorys))
 	// --> link
-	http.HandleFunc("/api/v1/addLink", route.AddLink)
-	http.HandleFunc("/api/v1/getLinks", route.GetLinks)
-	http.HandleFunc("/api/v1/getLinksByCategoryId", route.GetLinksByCategoryId)
+	http.HandleFunc("/api/v1/addLink", logger(route.AddLink))
+	http.HandleFunc("/api/v1/getLinks", logger(route.GetLinks))
+	http.HandleFunc("/api/v1/getLinksByCategoryId", logger(route.GetLinksByCategoryId))
 
 	// 启动服务
 	err := http.ListenAndServe(":1205", nil)
